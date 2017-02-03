cd510f4afe65bdd06877845f22630c3c4f2bde48
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-02-03 23:10:36 +0900

    rails update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_chat-space_session'
+Rails.application.config.session_store :cookie_store, key: '_chat_space_session'
