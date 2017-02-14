ec7540bce7521a9020fced79fcba748c9ca80598
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-02-14 16:57:55 +0900

    column index delete

--- db/migrate/20170214060917_create_groups.rb
+++ db/migrate/20170214060917_create_groups.rb
@@ -1,7 +1,9 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-    	t.string :name, index: true,  null: false, default: ""
+    	t.string :name,  null: false, default: ""
+    	#↑今回グループでグループ名の名前検索は行わないのでindexは貼らない。
+    	#indexとはよく検索するカラムにインデックスを貼ることで、検索速度を向上させるもの。多用しすぎると検索が逆に遅くなるので、データベースの設計が重要。
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 20170214060917) do
     t.string   "name",       default: "", null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
-    t.index ["name"], name: "index_groups_on_name", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
