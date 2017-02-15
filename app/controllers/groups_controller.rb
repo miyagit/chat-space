1e5cff681abc3f892065bdb00d96c2cd353c3862
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-02-15 09:37:54 +0900

    groupテーブルにレコードを追加できるようにしました。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,16 @@
 class GroupsController < ApplicationController
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(group_params)
+  end
+
+  private
+  def group_params
+  	params.require(:group).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
+  get 'groups/new' => 'groups#new'
+  post 'groups/'   => 'groups#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
