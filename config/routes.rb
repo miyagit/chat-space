b11932b9943209b7aedf1aea49c583d15b2e9f47
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-02-15 17:52:43 +0900

    group create fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  get 'groups/new' => 'groups#new'
-  post 'groups/'   => 'groups#create'
+  resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
