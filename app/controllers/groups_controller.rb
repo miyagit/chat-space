b981d5398e859394779219674175bfb7b0781463
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-02-17 18:44:16 +0900

    fix redirect

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,7 +27,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to edit_group_path, notice: "グループを変更しました。"
+      redirect_to edit_group_path(@group), notice: "グループを変更しました。"
     else
       flash[:alert] = "グループ名にデータを入力していないので保存できませんでした。"
       render :edit
