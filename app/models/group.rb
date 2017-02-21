ba438d2975240e6df44d35b49e5333c64c77456b
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-02-21 20:40:16 +0900

    グループの最新の投稿を追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,14 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :groups_users
   has_many :users, through: :groups_users
+
+  def latest_message
+    last_message = messages.last
+    if last_message
+    	return last_message.body
+    else
+    	return "このグループに投稿はありません"
+    end
+  end
+
 end
