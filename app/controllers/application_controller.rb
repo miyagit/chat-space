9dd4d699e211f824a9966dbe4195b01ef66d42d3
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-02-21 18:35:48 +0900

    throw away garbadge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-	layout 'application', if: :devise_controller?
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
