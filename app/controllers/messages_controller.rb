1fb98ff950427f50aaaea75630430d83cce3a491
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-02-21 20:14:02 +0900

    fix others2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
-  before_action :set_group, :set_groups, :set_message, :set_messages, only:[:index, :create]
+  before_action :set_groups, :set_messages, only:[:index, :create]
 
   def index
   end
 
   def create
-    @message = Message.new(create_params)
+    @message = current_user.messages.new(create_params)
     if @message.save
       redirect_to group_messages_path, notice: "メッセージを送信できました。"
     else
@@ -17,22 +17,16 @@ class MessagesController < ApplicationController
   private
 
   def create_params
-    params.require(:message).permit(:body).merge(user_id: current_user.id, group_id: params[:group_id])
-  end
-
-  def set_group
-    @group = Group.find(params[:group_id])
+    params.require(:message).permit(:body).merge(group_id: params[:group_id])
   end
 
   def set_groups
+    @group = Group.find(params[:group_id])
     @groups = current_user.groups
   end
 
-  def set_message
-    @message = Message.new
-  end
-
   def set_messages
+    @message = Message.new
     @messages = @group.messages
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,9 @@ class Message < ApplicationRecord
   validates :body, presence: true
   belongs_to :user
   belongs_to :group
+
+  def message_time
+  	created_at.to_s(:default)
+  end
+
 end
