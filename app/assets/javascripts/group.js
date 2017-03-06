7632ee6f5ebdbd113f4fe6a3f07d13560fc681bf
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-03-06 22:51:05 +0900

    追加，削除実装

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,19 +1,28 @@
 $(function() {
   preWord = 0;
   function buildHTML(users) {
-    console.log(users);
     var list = $("#list");
 
     $.each(users, function(i, user) {
-      var chat_group_user__btn = $('<button type="button" class="chat-group-user__btn">').append('<span class="chat-group-user__btn--add">').append("追加");
+      var chat_group_user__btn = $('<button type="button" class="chat-group-user__btn">').append('<span class="chat-group-user__btn--add">追加');
       var item = $('<li class="list">').append(user.name).append(chat_group_user__btn);
       list.append(item);
     });
-    $('.added-btn').on('click', function() {
+
+    $('.chat-group-user__btn').on('click', function() {
       var listing = $(this).parent();
+      var btn = listing.find('.chat-group-user__btn--add')
+      btn.text('削除');
+      btn.removeClass('chat-group-user__btn--add');
+      btn.addClass('chat-group-user__btn--remove');
       var memberlist = $("#memberlist");
       memberlist.append(listing);
-    })
+      $('.chat-group-user__btn--remove').on('click', function() {
+        var removelist = $(this).parents(".list");
+        removelist.remove();
+      });
+    });
+
     preWord = $("#chatments").val();
   };
 
