aa1966a2c1d21b401bad923cb3f9b24534110beb
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-03-21 17:33:53 +0900

    aws setting3

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/chatspace.pem`]
+                  keys: ['~/.ssh/chatspace.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
