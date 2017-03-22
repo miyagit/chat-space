f5e0d0bbd3198d755f9bd36f09dadaeef08be7e3
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-03-23 03:11:01 +0900

    fix carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    aws_access_key_id: ENV['ACCESS_KEY_S3_ID'],
+    aws_secret_access_key: ENV['SECRET_ACCESS_S3_KEY'],
     region: 'us-east-1'
   }
 
