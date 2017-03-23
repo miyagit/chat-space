ddb3411bda2246bae63f57bcde85c8780ece24da
Yuya Miyajima <yuya@miyajimaisamuwatarunoMacBook.local>
2017-03-23 17:00:01 +0900

    unicorn change

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
 working_directory "#{app_path}/current"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
